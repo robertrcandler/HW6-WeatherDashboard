7a8a7cc36e88c4387582d087be7df5a89a547f43
Robert Candler <robert.candler.32@gmail.com>
2020-04-14 19:40:51 -0600

    everything but the search bar

--- logic.js
+++ logic.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
     var todayURL = "https://api.openweathermap.org/data/2.5/weather?appid=e6abac337ca67955353882fbffaec4c2&q=" + cityname;
     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=e6abac337ca67955353882fbffaec4c2&q=" + cityname;
     var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=e6abac337ca67955353882fbffaec4c2&q=" + cityname;
-    console.log(forecastURL);
     
     //variables for today
     $.ajax({
@@ -27,9 +26,14 @@ $(document).ready(function () {
           method: "GET"
         }).then(function(response) {
           var UVindex = "<p>UV Index: " + response.value + "</p>";
-          
+          //get today's date
+          var d = new Date();
+          var month = d.getMonth() + 1;
+          var day = d.getDate();
+          var year = d.getFullYear();
+          var date0 = "(" + month + "/" + day + "/" + year + ")";
           //append the variables of today
-          $("#Title").html("<h2>" + cityname + " (1/11/1111) " + icon0 + "</h2>");
+          $("#Title").html("<h2>" + cityname + " " + date0 + " " + icon0 + "</h2>");
           $("#day0temp").html(tempF0);
           $("#day0hunid").html(humid0);
           $("#day0wind").html(wind0);
@@ -42,37 +46,26 @@ $(document).ready(function () {
       url: forecastURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
-      var icon1 =  "<img src='https://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + "@2x.png' >";
-      var temp1 = (Math.round((((response.list[1].main.temp-273.15)*1.8+32) + Number.EPSILON) * 10) / 10) + " F";
-      var humid1 = response.list[1].main.humidity + "%";
-      var icon2 =  "<img src='https://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png' >";
-      var temp2 = (Math.round((((response.list[2].main.temp-273.15)*1.8+32) + Number.EPSILON) * 10) / 10) + " F";
-      var humid2 = response.list[2].main.humidity + "%";
-      var icon3 =  "<img src='https://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + "@2x.png' >";
-      var temp3 = (Math.round((((response.list[3].main.temp-273.15)*1.8+32) + Number.EPSILON) * 10) / 10) + " F";
-      var humid3 = response.list[3].main.humidity + "%";
-      var icon4 =  "<img src='https://openweathermap.org/img/wn/" + response.list[4].weather[0].icon + "@2x.png' >";
-      var temp4 = (Math.round((((response.list[4].main.temp-273.15)*1.8+32) + Number.EPSILON) * 10) / 10) + " F";
-      var humid4 = response.list[4].main.humidity + "%";
-      var icon5 =  "<img src='https://openweathermap.org/img/wn/" + response.list[5].weather[0].icon + "@2x.png' >";
-      var temp5 = (Math.round((((response.list[5].main.temp-273.15)*1.8+32) + Number.EPSILON) * 10) / 10) + " F";
-      var humid5 = response.list[5].main.humidity + "%";
-
+      //recreate variables for the time display
+      var d = new Date();
+      var month = d.getMonth() + 1;
+      var year = d.getFullYear();
       //append variables for rest of week
       for (var i=1; i<6; i++) {
         //make variables
-        // var daydate = '(' + (today.getMonth() + 1) + '/' + (today.getDate()+1+i) + '/' + today.getFullYear() + ')';
+        var day = d.getDate() + i;
+        var date = "(" + month + "/" + day + "/" + year + ")";
         var icon =  "<p><img src='https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'></p>"
         var temp = "<p>Temperature: " + (Math.round((((response.list[i].main.temp-273.15)*1.8+32) + Number.EPSILON) * 10) / 10) + " F</p>";
         var humid = "<p>Humidiy: " + response.list[i].main.humidity + "%</p>";
-        // //append variables
+        //make html variable names
         var idate = "#" + JSON.stringify(i) + "date";
         var iicon = "#" + JSON.stringify(i) + "icon";
         var itemp = "#" + JSON.stringify(i) + "temp";
         var ihumid = "#" + JSON.stringify(i) + "humid";
-
+        //append variables
         // $(jdate).html("<strong>" + daydate + "</strong>");
+        $(idate).html(date);
         $(iicon).html(icon);
         $(itemp).html(temp);
         $(ihumid).html(humid);
@@ -81,7 +74,23 @@ $(document).ready(function () {
     });
   }
   APIuse("Austin");
-
-
+  //create functional search button
+  $(document).ready(function() {
+    $('#searchbtn').click(function() {
+      var input = $("#searcharea").val();
+        APIuse(input);
+    });
+  });
+  //create clickable buttons to change the displayed city
+  $(document).ready(function() {
+    $('#city1').click(function() {APIuse("Austin")});
+    $('#city2').click(function() {APIuse("Chicago")});
+    $('#city3').click(function() {APIuse("New York")});
+    $('#city4').click(function() {APIuse("Orlando")});
+    $('#city5').click(function() {APIuse("San Francisco")});
+    $('#city6').click(function() {APIuse("Seattle")});
+    $('#city7').click(function() {APIuse("Denver")});
+    $('#city8').click(function() {APIuse("Atlanta")});
+  });
 });
 
